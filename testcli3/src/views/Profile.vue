6e631ad71c3d7196fcc81d741d45a5a9420494aa
CXY0501 <chen.xy.2@foxmail.com>
2021-04-10 10:41:08 +0800

    202104010-2

---
+++ testcli3/src/views/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="Profile">
+    <h1>This is Profile Page</h1>
+    <p>yeah!!!!!!!!</p>
+  </div>
+</template>
+
+<script>
+export default{
+  name:'Profile',
+  data(){
+    return{}
+  },
+  methods:{
+  }
+}
+</script>
+
+<style scoped>
+</style>
